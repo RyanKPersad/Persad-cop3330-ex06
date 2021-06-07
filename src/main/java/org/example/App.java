56b4e9d157b578cd6c6c08060349646968c74c9a
ryanpersad <Rpersad0305@gmail.com>
2021-06-06 20:12:32 -0400

    update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,11 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Ryan Persad
+ */
 package org.example;
 import java.time.*;
 import java.util.Scanner;
 
-/**
- *
- */
 public class App 
 {
     public static void main( String[] args )
